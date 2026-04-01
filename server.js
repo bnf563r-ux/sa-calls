f25efe4afd0cc30e20b66b459b622a04a59c91b9
bnf563r-ux <bnf563r@gmail.com>
2026-04-01 10:46:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,14 +5,13 @@ const io = require("socket.io")(http);
 
 app.use(express.static("public"));
 
-io.on("connection", (socket) => {
-    console.log("User connected: " + socket.id);
-
-    socket.on("join-room", (roomId) => {
+io.on("connection", socket => {
+    socket.on("join-room", roomId => {
         socket.join(roomId);
+
         socket.to(roomId).emit("user-joined", socket.id);
 
-        socket.on("signal", (data) => {
+        socket.on("signal", data => {
             io.to(data.to).emit("signal", {
                 from: socket.id,
                 signal: data.signal
@@ -25,6 +24,7 @@ io.on("connection", (socket) => {
     });
 });
 
-http.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
